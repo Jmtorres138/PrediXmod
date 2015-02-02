40da19a5d642e7d7817ed0e9075dee6983b9c53d
Jason Torres <jason.matthew.torres@gmail.com>
2015-02-02 17:08:04 -0600

    moved job_1 script to job_out directory

---
+++ job_out/job_1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#PBS -N vcf_conversion 
+#PBS -S /bin/bash
+#PBS -l mem=40gb
+#PBS -o $HOME/group/im-lab/nas40t2/jason/projects/PrediXmod/job_out/job_1.out
+#PBS -e $HOME/group/im-lab/nas40t2/jason/projects/PrediXmod/job_out/job_1.err
+
+
+perl /group/im-lab/nas40t2/jason/projects/PrediXmod/1_vcf2dosage.mach_gtex_hapmapSNPs.pl 
