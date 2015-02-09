25f70fd90651856134fea05bba34cd22d88afbcd
Jason Torres <jason.matthew.torres@gmail.com>
2015-02-08 21:38:36 -0600

    job_1 Starr County and Mexico City Merge SNPs

--- job_out/job_1.sh
+++ job_out/job_1.sh
@@ -2,8 +2,8 @@
 #PBS -N vcf_conversion 
 #PBS -S /bin/bash
 #PBS -l mem=40gb
-#PBS -o $HOME/group/im-lab/nas40t2/jason/projects/PrediXmod/job_out/job_1.out
-#PBS -e $HOME/group/im-lab/nas40t2/jason/projects/PrediXmod/job_out/job_1.err
+#PBS -o $HOME/group/im-lab/nas40t2/jason/projects/PrediXmod/job_out/job_1_Mex.out
+#PBS -e $HOME/group/im-lab/nas40t2/jason/projects/PrediXmod/job_out/job_1_Mex.err
 
 
-perl /group/im-lab/nas40t2/jason/projects/PrediXmod/1_vcf2dosage.mach_gtex_hapmapSNPs.pl 
+perl /group/im-lab/nas40t2/jason/projects/PrediXmod/1_vcf2dosage.mach_gtex_MexSNPs.pl 
